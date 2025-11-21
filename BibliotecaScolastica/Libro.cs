c804a896070b6db2bcc06998e98bacbd12fcf468
Matteo Boschi <MATTEO.BOSCHI@local.ittcesena.it>
2025-11-21 08:34:14 +0100

    Aggiunto costruttore e metodo ToString

--- BibliotecaScolastica/Libro.cs
+++ BibliotecaScolastica/Libro.cs
@@ -8,11 +8,25 @@ namespace BibliotecaScolastica
 {
     internal class Libro
     {
-        public string Autore { get; private set; }
-        public string Titolo { get; private set; }
-        public DateTime AnnoDiPubblicazione { get; private set; }
-        public string Editore { get; private set; }
-        public int NumeroDiPagine { get; private set; }
+        public string Autore { get; set; }
+        public string Titolo { get; set; }
+        public DateTime AnnoDiPubblicazione { get; set; }
+        public string Editore { get; set; }
+        public int NumeroDiPagine { get; set; }
+
+        public Libro(string autore, string titolo, DateTime annoDiPubblicazione, string editore, int numeroDiPagine)
+        {
+            Autore = autore;
+            Titolo = titolo;
+            AnnoDiPubblicazione = annoDiPubblicazione;
+            Editore = editore;
+            NumeroDiPagine = numeroDiPagine;
+        }
+
+        public override string ToString()
+        {
+            return $"{Autore}, {Titolo}, {AnnoDiPubblicazione}, {Editore}, {NumeroDiPagine}";
+        }
 
     }
 }
